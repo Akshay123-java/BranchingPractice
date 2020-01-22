2c1592ca16ddf15be363e3e2f24f0ba311930650
Akshay123-java <akshaychidre1995@gmail.com>
2020-01-22 16:41:08 +0530

    branching practice

---
+++ src/com/example/TestExample.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class TestExample {
+
+ static  int i=5;
+   
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(i);
+		
+	}
+
+}
